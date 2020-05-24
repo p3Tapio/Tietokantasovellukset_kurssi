071ee72fc0dd3d61a3a2146863fce7032a62dc69
p3Tapio <petri.laine@student.careeria.fi>
2020-05-24 15:39:12 +0300

    Henkiöiden sorteeraus siivottu omaan luokkaan, JQ downgrade koska burgeri lakkasi toimimasta 3.5-versiolla

--- TilausASPNET/Controllers/HenkilotController.cs
+++ TilausASPNET/Controllers/HenkilotController.cs
@@ -5,11 +5,9 @@ using System.Data.Entity;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using TilausASPNET.Models;
 using TilausASPNET.Helpperi;
-using System.Web.UI;
 using PagedList;
 
 namespace TilausASPNET.Controllers
@@ -22,7 +20,6 @@ namespace TilausASPNET.Controllers
         // GET: Henkilot
         public ActionResult Index(string sortOrder, string searchString1, string currentFilter1, string PostinumeroHaku, string currentPostinumero, int? page, int? pagesize)
         {
-            // TODO: Pilko tätä aliRutiineihin()  !! !! !! !! 
 
             if (Session["UserName"] == null)
             {
@@ -37,23 +34,14 @@ namespace TilausASPNET.Controllers
                 ViewBag.SukunimiSortParam = string.IsNullOrEmpty(sortOrder) ? "sukunimi_desc" : "";
                 ViewBag.EtunimiSortParam = sortOrder == "etunimi" ? "etunimi_desc" : "etunimi";         // = sortOrder == --> Ilmeisesti vertailu riippuu siitä oliko sortOrder ennestään etunimi 
 
-                if (searchString1 != null)
-                {
-                    page = 1;
-                }
-                else
-                {
-                    searchString1 = currentFilter1;
-                }
+                if (searchString1 != null) page = 1;
+                else searchString1 = currentFilter1;
+
                 ViewBag.currentFilter1 = searchString1;
-                if (PostinumeroHaku != null && PostinumeroHaku != "0")
-                {
-                    page = 1;
-                }
-                else
-                {
-                    PostinumeroHaku = currentPostinumero;
-                }
+
+                if (PostinumeroHaku != null && PostinumeroHaku != "0") page = 1;
+                else PostinumeroHaku = currentPostinumero;
+
 
                 ViewBag.currentPostinumero = PostinumeroHaku;
 
@@ -65,73 +53,24 @@ namespace TilausASPNET.Controllers
                     henkilot = henkilot.Where(x => x.Postinumero == PostinumeroHaku);
                 }
 
-                // ALIRUTIINIIN, iffitys siellä tai eri rutiinit per if - else if - else :
-                // voisi kääntää kans switchiin 
                 if (!String.IsNullOrEmpty(searchString1))   // jos hakufiltteri käytössä, niin Where:n lisäksi lajitellaan tulokset
                 {
-
-                    switch (sortOrder)
-                    {
-                        case "sukunimi_desc":
-                            henkilot = henkilot.Where(x => x.Sukunimi.Contains(searchString1)).OrderByDescending(x => x.Sukunimi);
-                            break;
-                        case "etunimi":
-                            henkilot = henkilot.Where(x => x.Sukunimi.Contains(searchString1)).OrderBy(x => x.Etunimi);
-                            break;
-                        case "etunimi_desc":
-                            henkilot = henkilot.Where(x => x.Sukunimi.Contains(searchString1)).OrderByDescending(x => x.Etunimi);
-                            break;
-                        default:
-                            henkilot = henkilot.Where(x => x.Sukunimi.Contains(searchString1)).OrderBy(x => x.Sukunimi);
-                            break;
-                    }
+                    henkilot = HenkilotFilters.FilterSortLastName(sortOrder, searchString1, henkilot);
                 }
                 else if (!String.IsNullOrEmpty(PostinumeroHaku) && (PostinumeroHaku != "0"))
                 {
-
-                    switch (sortOrder)
-                    {
-                        case "sukunimi_desc":
-                            henkilot = henkilot.Where(x => x.Postinumero == PostinumeroHaku).OrderByDescending(x => x.Sukunimi);
-                            break;
-                        case "etunimi":
-                            henkilot = henkilot.Where(x => x.Postinumero == PostinumeroHaku).OrderBy(x => x.Etunimi);
-                            break;
-                        case "etunimi_desc":
-                            henkilot = henkilot.Where(x => x.Postinumero == PostinumeroHaku).OrderByDescending(x => x.Etunimi);
-                            break;
-                        default:
-                            henkilot = henkilot.Where(x => x.Postinumero == PostinumeroHaku).OrderBy(x => x.Sukunimi);
-                            break;
-                    }
-
+                    henkilot = HenkilotFilters.FilterSortPostinumero(sortOrder, PostinumeroHaku, henkilot);
                 }
                 else    // Jos ei filtteriä, pelkkä lajittelu 
                 {
-
-                    switch (sortOrder)
-                    {
-                        case "sukunimi_desc":
-                            henkilot = henkilot.OrderByDescending(x => x.Sukunimi);
-                            break;
-                        case "etunimi":
-                            henkilot = henkilot.OrderBy(x => x.Etunimi);
-                            break;
-                        case "etunimi_desc":
-                            henkilot = henkilot.OrderByDescending(x => x.Etunimi);
-                            break;
-                        default:
-                            henkilot = henkilot.OrderBy(x => x.Sukunimi);
-                            break;
-                    }
+                    henkilot = HenkilotFilters.NoFilterSortHenkilot(sortOrder, henkilot);
 
                 }
+
                 var postiLista = from x in db.Postitoimipaikat select x;
-                var postiSelectList = PostiDropDownList(postiLista);    // source, key, sisalto, metodin parametri, jonka nimesin kuten cshtml:ssä hidden-kentän
+                var postiSelectList = HenkilotFilters.PostiDropDownList(postiLista);    // source, key, sisalto, metodin parametri, jonka nimesin kuten cshtml:ssä hidden-kentän
 
                 ViewBag.Postinumero = new SelectList(postiSelectList, "Postinumero", "PostiNroPaikka", PostinumeroHaku);
-
-
 
                 int pageSize = (pagesize ?? 10);
                 int pageNumber = (page ?? 1);
@@ -139,31 +78,6 @@ namespace TilausASPNET.Controllers
                 return View(henkilot.Include(x => x.Postitoimipaikat).ToPagedList(pageNumber, pageSize));
             }
         }
-
-
-        private List<Postitoimipaikat> PostiDropDownList(IQueryable<Postitoimipaikat> lista)
-        {
-            // esimissä on ID int keynä ...
-            // tässä Postinumero on string -- 
-
-            List<Postitoimipaikat> palatuva = new List<Postitoimipaikat>();
-            Postitoimipaikat blankko = new Postitoimipaikat { Postinumero = "", Postitoimipaikka = "", PostiNroPaikka = "Valitse hakuehto" };
-            palatuva.Add(blankko);
-
-            foreach (Postitoimipaikat postitoim in lista)
-            {
-                Postitoimipaikat x = new Postitoimipaikat
-                {
-                    Postinumero = postitoim.Postinumero,
-                    Postitoimipaikka = postitoim.Postitoimipaikka,
-                    PostiNroPaikka = postitoim.Postinumero + " - " + postitoim.Postitoimipaikka
-                };
-                Debug.WriteLine(x);
-                palatuva.Add(x);
-            }
-            return palatuva;
-        }
-
 
         // GET: Henkilot/Details/5
         public ActionResult Details(int? id)
@@ -331,22 +245,22 @@ namespace TilausASPNET.Controllers
             Debug.Write("Disposing....");
             base.Dispose(disposing);
         }
-        private void LuoRandomHenkilo()
-        {
-            string etunimi = HenkiloGenerator.Etunimi();
-            string sukunimi = HenkiloGenerator.Sukunimi();
+        //private void LuoRandomHenkilo()
+        //{
+        //    string etunimi = HenkiloGenerator.Etunimi();
+        //    string sukunimi = HenkiloGenerator.Sukunimi();
 
-            Henkilot newHlo = new Henkilot
-            {
-                Etunimi = etunimi,
-                Sukunimi = HenkiloGenerator.Sukunimi(),
-                Osoite = HenkiloGenerator.Osoite(),
-                Sahkoposti = etunimi.ToLower() + "." + sukunimi.ToLower() + HenkiloGenerator.Sposti(),
-                Postinumero = HenkiloGenerator.Postinumero(),
-                Esimies = HenkiloGenerator.Esimies()
-            };
-            db.Henkilot.Add(newHlo);
-            db.SaveChanges();
-        }
+        //    Henkilot newHlo = new Henkilot
+        //    {
+        //        Etunimi = etunimi,
+        //        Sukunimi = HenkiloGenerator.Sukunimi(),
+        //        Osoite = HenkiloGenerator.Osoite(),
+        //        Sahkoposti = etunimi.ToLower() + "." + sukunimi.ToLower() + HenkiloGenerator.Sposti(),
+        //        Postinumero = HenkiloGenerator.Postinumero(),
+        //        Esimies = HenkiloGenerator.Esimies()
+        //    };
+        //    db.Henkilot.Add(newHlo);
+        //    db.SaveChanges();
+        //}
     }
 }
---
+++ TilausASPNET/Helpperi/HenkilotFilters.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TilausASPNET.Models;
+
+namespace TilausASPNET.Helpperi
+{
+    public class HenkilotFilters
+    {
+        public static List<Postitoimipaikat> PostiDropDownList(IQueryable<Postitoimipaikat> lista)
+        {
+
+            List<Postitoimipaikat> palatuva = new List<Postitoimipaikat>();
+            Postitoimipaikat blankko = new Postitoimipaikat { Postinumero = "", Postitoimipaikka = "", PostiNroPaikka = "Valitse hakuehto" };
+            palatuva.Add(blankko);
+
+            foreach (Postitoimipaikat postitoim in lista)
+            {
+                Postitoimipaikat x = new Postitoimipaikat
+                {
+                    Postinumero = postitoim.Postinumero,
+                    Postitoimipaikka = postitoim.Postitoimipaikka,
+                    PostiNroPaikka = postitoim.Postinumero + " - " + postitoim.Postitoimipaikka
+                };
+
+                palatuva.Add(x);
+            }
+            return palatuva;
+        }
+        public static IQueryable<Henkilot> FilterSortLastName(string sortOrder, string searchString1, IQueryable<Henkilot> henkilot)
+        {
+            switch (sortOrder)
+            {
+                case "sukunimi_desc":
+                    henkilot = henkilot.Where(x => x.Sukunimi.Contains(searchString1)).OrderByDescending(x => x.Sukunimi);
+                    break;
+                case "etunimi":
+                    henkilot = henkilot.Where(x => x.Sukunimi.Contains(searchString1)).OrderBy(x => x.Etunimi);
+                    break;
+                case "etunimi_desc":
+                    henkilot = henkilot.Where(x => x.Sukunimi.Contains(searchString1)).OrderByDescending(x => x.Etunimi);
+                    break;
+                default:
+                    henkilot = henkilot.Where(x => x.Sukunimi.Contains(searchString1)).OrderBy(x => x.Sukunimi);
+                    break;
+
+            }
+            return henkilot;
+        }
+        public static IQueryable<Henkilot> FilterSortPostinumero(string sortOrder, string PostinumeroHaku, IQueryable<Henkilot> henkilot)
+        {
+            switch (sortOrder)
+            {
+                case "sukunimi_desc":
+                    henkilot = henkilot.Where(x => x.Postinumero == PostinumeroHaku).OrderByDescending(x => x.Sukunimi);
+                    break;
+                case "etunimi":
+                    henkilot = henkilot.Where(x => x.Postinumero == PostinumeroHaku).OrderBy(x => x.Etunimi);
+                    break;
+                case "etunimi_desc":
+                    henkilot = henkilot.Where(x => x.Postinumero == PostinumeroHaku).OrderByDescending(x => x.Etunimi);
+                    break;
+                default:
+                    henkilot = henkilot.Where(x => x.Postinumero == PostinumeroHaku).OrderBy(x => x.Sukunimi);
+                    break;
+            }
+            return henkilot;
+        }
+        public static IQueryable<Henkilot> NoFilterSortHenkilot(string sortOrder, IQueryable<Henkilot> henkilot)
+        {
+
+            switch (sortOrder)
+            {
+                case "sukunimi_desc":
+                    henkilot = henkilot.OrderByDescending(x => x.Sukunimi);
+                    break;
+                case "etunimi":
+                    henkilot = henkilot.OrderBy(x => x.Etunimi);
+                    break;
+                case "etunimi_desc":
+                    henkilot = henkilot.OrderByDescending(x => x.Etunimi);
+                    break;
+                default:
+                    henkilot = henkilot.OrderBy(x => x.Sukunimi);
+                    break;
+            }
+
+            return henkilot;
+        }
+
+    }
+}
